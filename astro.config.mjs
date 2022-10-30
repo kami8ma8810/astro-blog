acb350c4817b853f0998b7289ebed48078d9006f
Hayato Kamiyama <kami8ma8810@gmail.com>
2022-10-30 14:26:27 +0900

    distテスト

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import image from "@astrojs/image";
+import image from '@astrojs/image';
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image()],
-  output: "server",
-  adapter: vercel()
+	integrations: [image()],
+	// output: "server",
+	// adapter: vercel()
 });
