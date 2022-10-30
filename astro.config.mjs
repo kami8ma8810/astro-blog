11a57552be021868972e87e4721e6d6d2e49452a
Hayato Kamiyama <kami8ma8810@gmail.com>
2022-10-30 17:34:21 +0900

    vercelへはCLIでデプロイ成功

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [image()],
-	// output: "server",
-	// adapter: vercel()
+	output: 'server',
+	adapter: vercel(),
 });
