820bdca6884c74bb93e4abea5fe04db8a182756b
Hayato Kamiyama <kami8ma8810@gmail.com>
2022-10-31 09:53:51 +0900

    astro-imagetools導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,11 @@ import image from '@astrojs/image';
 
 // https://astro.build/config
 import vercel from '@astrojs/vercel/serverless';
+import { astroImageTools } from 'astro-imagetools';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [image()],
+	integrations: [image(), astroImageTools],
 	output: 'server',
 	adapter: vercel(),
 });
